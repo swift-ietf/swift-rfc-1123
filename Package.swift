269c6fcd528edb5c41f573a72f46ddc4c09b91e9
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-08-20 11:11:57 +0200

    Format and require Swift 6.4

--- Package.swift
+++ Package.swift
@@ -1,8 +1,6 @@
-// swift-tools-version: 6.3.3
+// swift-tools-version: 6.4
 
 import PackageDescription
-
-
 
 extension String {
     static let rfc1123: Self = "RFC 1123"
@@ -11,28 +9,43 @@ extension String {
 extension Target.Dependency {
     static var rfc1123: Self { .target(name: .rfc1123) }
     static var rfc1035: Self { .product(name: "RFC 1035", package: "swift-rfc-1035") }
-    static var standards: Self { .product(name: "Standard Library Extensions", package: "swift-standard-library-extensions") }
-    static var incits41986: Self { .product(name: "ASCII Serializer Primitives", package: "swift-ascii-serializer-primitives") }
-    static var asciiParser: Self { .product(name: "Parseable ASCII Primitives", package: "swift-ascii-parser-primitives") }
+    static var standards: Self {
+        .product(name: "Standard Library Extensions", package: "swift-standard-library-extensions")
+    }
+    static var incits41986: Self {
+        .product(name: "ASCII Serializer Primitives", package: "swift-ascii-serializer-primitives")
+    }
+    static var asciiParser: Self {
+        .product(name: "Parseable ASCII Primitives", package: "swift-ascii-parser-primitives")
+    }
 }
 
 let package = Package(
     name: "swift-rfc-1123",
     platforms: [
-        .macOS("27"),
-        .iOS("27"),
-        .tvOS("27"),
-        .watchOS("27"),
-        .visionOS("27")
+        .macOS(.v27),
+        .iOS(.v27),
+        .tvOS(.v27),
+        .watchOS(.v27),
+        .visionOS(.v27),
     ],
     products: [
         .library(name: "RFC 1123", targets: ["RFC 1123"])
     ],
     dependencies: [
         .package(url: "https://github.com/swift-ietf/swift-rfc-1035.git", branch: "main"),
-        .package(url: "https://github.com/swift-primitives/swift-standard-library-extensions.git", branch: "main"),
-        .package(url: "https://github.com/swift-primitives/swift-ascii-serializer-primitives.git", branch: "main"),
-        .package(url: "https://github.com/swift-primitives/swift-ascii-parser-primitives.git", branch: "main")
+        .package(
+            url: "https://github.com/swift-primitives/swift-standard-library-extensions.git",
+            branch: "main"
+        ),
+        .package(
+            url: "https://github.com/swift-primitives/swift-ascii-serializer-primitives.git",
+            branch: "main"
+        ),
+        .package(
+            url: "https://github.com/swift-primitives/swift-ascii-parser-primitives.git",
+            branch: "main"
+        ),
     ],
     targets: [
         .target(
@@ -41,13 +54,13 @@ let package = Package(
                 .rfc1035,
                 .standards,
                 .incits41986,
-                .asciiParser
+                .asciiParser,
             ]
         ),
         .testTarget(
             name: "RFC 1123 Tests",
             dependencies: [
-                "RFC 1123",
+                "RFC 1123"
             ]
         ),
     ],
